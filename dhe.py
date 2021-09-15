32f9806576dec736974e3bd84f8ac193837aed5e
Abhibha Gupta <abhibha1807@gmail.com>
2021-09-15 17:34:42 +0530

    job 9, perm change

--- dhe.py
+++ dhe.py
@@ -268,11 +268,31 @@ def main():
     #     cv2.imwrite(towrite+i, result)
     # print('cleaned test copy NORMAL finished')
     
+    # c=0
+    # for i in onlyfiles8:
+    #     c=c+1
+    #     towrite = './cleaned_test_copy/PNEUMONIA/'
+    #     img_name = filename8 + i
+    #     img = imageio.imread(img_name)
+    #     img_ = cv2.imread(img_name)
+    #     #plt.imshow(img_)
+    #     l=img.shape[0]
+    #     w=img.shape[1]
+        
+    #     img = img_.reshape(l,w,3)
+    #     print(img.shape)
+    #     result = dhe(img)
+    #     #plt.imshow(result)
+    #     #plt.show()
+    #     cv2.imwrite(towrite+i, result)
+    #     print(c)
+    # print('cleaned test copy PNEUMONIA finished')
+    
     c=0
-    for i in onlyfiles8:
-        c=c+1
-        towrite = './cleaned_test_copy/PNEUMONIA/'
-        img_name = filename8 + i
+    for i in onlyfiles9:
+        c=c=1
+        towrite = './cleaned_val_copy/NORMAL/'
+        img_name = filename9 + i
         img = imageio.imread(img_name)
         img_ = cv2.imread(img_name)
         #plt.imshow(img_)
@@ -286,23 +306,7 @@ def main():
         #plt.show()
         cv2.imwrite(towrite+i, result)
         print(c)
-    print('cleaned test copy PNEUMONIA finished')
-    
-    # for i in onlyfiles9:
-    #     towrite = './cleaned_val_copy/NORMAL/'
-    #     img_name = filename9 + i
-    #     img = imageio.imread(img_name)
-    #     img_ = cv2.imread(img_name)
-    #     #plt.imshow(img_)
-    #     l=img.shape[0]
-    #     w=img.shape[1]
-        
-    #     img = img_.reshape(l,w,3)
-    #     print(img.shape)
-    #     result = dhe(img)
-    #     #plt.imshow(result)
-    #     #plt.show()
-    #     cv2.imwrite(towrite+i, result)
+    print('cleaned val copy NORMAL finished')
     
     # for i in onlyfiles10:
     #     towrite = './cleaned_val_copy/PNEUMONIA/'
