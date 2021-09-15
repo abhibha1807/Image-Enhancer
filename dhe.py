a77ab379defc75c835229d591d8c902a6f9971a9
Abhibha Gupta <abhibha1807@gmail.com>
2021-09-16 00:58:22 +0530

    job7, >91

--- dhe.py
+++ dhe.py
@@ -249,24 +249,26 @@ def main():
     #     cv2.imwrite(towrite+i, result)
     # print('cleaned train copy PNEUMONIA finished')
 
-    # c=0
-    # for i in onlyfiles7:
-    #     c=c+1
-    #     towrite = './cleaned_test_copy/NORMAL/'
-    #     img_name = filename7 + i
-    #     img = imageio.imread(img_name)
-    #     img_ = cv2.imread(img_name)
-    #     #plt.imshow(img_)
-    #     l=img.shape[0]
-    #     w=img.shape[1]
-        
-    #     img = img_.reshape(l,w,3)
-    #     print(img.shape)
-    #     result = dhe(img)
-    #     #plt.imshow(result)
-    #     #plt.show()
-    #     cv2.imwrite(towrite+i, result)
-    # print('cleaned test copy NORMAL finished')
+    c=0
+    for i in onlyfiles7:
+        c=c+1
+        if c>91:
+            towrite = './cleaned_test_copy/NORMAL/'
+            img_name = filename7 + i
+            img = imageio.imread(img_name)
+            img_ = cv2.imread(img_name)
+            #plt.imshow(img_)
+            l=img.shape[0]
+            w=img.shape[1]
+            
+            img = img_.reshape(l,w,3)
+            print(img.shape)
+            result = dhe(img)
+            #plt.imshow(result)
+            #plt.show()
+            cv2.imwrite(towrite+i, result)
+            print(c)
+    print('cleaned test copy NORMAL finished')
     
     # c=0
     # for i in onlyfiles8:
@@ -308,25 +310,25 @@ def main():
     #     print(c)
     # print('cleaned val copy NORMAL finished')
     
-    c=0
-    for i in onlyfiles10:
-        c=c+1
-        towrite = './cleaned_val_copy/PNEUMONIA/'
-        img_name = filename10 + i
-        img = imageio.imread(img_name)
-        img_ = cv2.imread(img_name)
-        #plt.imshow(img_)
-        l=img.shape[0]
-        w=img.shape[1]
+    # c=0
+    # for i in onlyfiles10:
+    #     c=c+1
+    #     towrite = './cleaned_val_copy/PNEUMONIA/'
+    #     img_name = filename10 + i
+    #     img = imageio.imread(img_name)
+    #     img_ = cv2.imread(img_name)
+    #     #plt.imshow(img_)
+    #     l=img.shape[0]
+    #     w=img.shape[1]
         
-        img = img_.reshape(l,w,3)
-        print(img.shape)
-        result = dhe(img)
-        #plt.imshow(result)
-        #plt.show()
-        cv2.imwrite(towrite+i, result)
-        print(c)
-    print('cleaned val copy PNEUMONIA finished')
+    #     img = img_.reshape(l,w,3)
+    #     print(img.shape)
+    #     result = dhe(img)
+    #     #plt.imshow(result)
+    #     #plt.show()
+    #     cv2.imwrite(towrite+i, result)
+    #     print(c)
+    # print('cleaned val copy PNEUMONIA finished')
        
 
 if __name__ == '__main__':
