732dad3e09b4d86dd0bc3e533306072bd38539a0
Abhibha Gupta <abhibha1807@gmail.com>
2021-09-15 14:32:07 +0530

    job 3, 2 gb

--- dhe.py
+++ dhe.py
@@ -166,25 +166,9 @@ def main():
     #     #plt.show()
     #     cv2.imwrite(towrite+i, result)
 
-    # for i in onlyfiles3:
-    #     towrite = './cleaned_test/NORMAL/'
-    #     img_name = filename3 + i
-    #     img = imageio.imread(img_name)
-    #     img_ = cv2.imread(img_name)
-    #     #plt.imshow(img_)
-    #     l=img.shape[0]
-    #     w=img.shape[1]
-        
-    #     img = img_.reshape(l,w,3)
-    #     print(img.shape)
-    #     result = dhe(img)
-    #     #plt.imshow(result)
-    #     #plt.show()
-    #     cv2.imwrite(towrite+i, result)
-    
-    for i in onlyfiles4:
-        towrite = './cleaned_test/PNEUMONIA/'
-        img_name = filename4 + i
+    for i in onlyfiles3:
+        towrite = './cleaned_test/NORMAL/'
+        img_name = filename3 + i
         img = imageio.imread(img_name)
         img_ = cv2.imread(img_name)
         #plt.imshow(img_)
@@ -197,6 +181,22 @@ def main():
         #plt.imshow(result)
         #plt.show()
         cv2.imwrite(towrite+i, result)
+    
+    # for i in onlyfiles4:
+    #     towrite = './cleaned_test/PNEUMONIA/'
+    #     img_name = filename4 + i
+    #     img = imageio.imread(img_name)
+    #     img_ = cv2.imread(img_name)
+    #     #plt.imshow(img_)
+    #     l=img.shape[0]
+    #     w=img.shape[1]
+        
+    #     img = img_.reshape(l,w,3)
+    #     print(img.shape)
+    #     result = dhe(img)
+    #     #plt.imshow(result)
+    #     #plt.show()
+    #     cv2.imwrite(towrite+i, result)
     
     # for i in onlyfiles5:
     #     towrite = './cleaned_train_copy/NORMAL/'
