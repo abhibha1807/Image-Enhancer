29f2ae1cbcdc192c3c7cd9971b8e863a7267ecf3
Abhibha Gupta <abhibha1807@gmail.com>
2021-09-15 14:35:53 +0530

    job1, 2 gb

--- dhe.py
+++ dhe.py
@@ -131,40 +131,40 @@ def main():
     onlyfiles10 = [f for f in listdir(filename10) if isfile(join(filename10, f))]
 
     #print(onlyfiles)
-    # c=0
-    # for i in onlyfiles1:
-    #     c=c+1
-    #     if c > 120:
-    #         towrite = './cleaned_train/NORMAL/'
-    #         img_name = filename1 + i
-    #         img = imageio.imread(img_name)
-    #         img_ = cv2.imread(img_name)
-    #         #plt.imshow(img_)
-    #         l=img.shape[0]
-    #         w=img.shape[1]
+    c=0
+    for i in onlyfiles1:
+        c=c+1
+        if c > 120:
+            towrite = './cleaned_train/NORMAL/'
+            img_name = filename1 + i
+            img = imageio.imread(img_name)
+            img_ = cv2.imread(img_name)
+            #plt.imshow(img_)
+            l=img.shape[0]
+            w=img.shape[1]
             
-    #         img = img_.reshape(l,w,3)
-    #         print(img.shape)
-    #         result = dhe(img)
-    #         #plt.imshow(result)
-    #         #plt.show()
-    #         cv2.imwrite(towrite+i, result)
+            img = img_.reshape(l,w,3)
+            print(img.shape)
+            result = dhe(img)
+            #plt.imshow(result)
+            #plt.show()
+            cv2.imwrite(towrite+i, result)
     
-    for i in onlyfiles2:
-        towrite = './cleaned_train/PNEUMONIA/'
-        img_name = filename2 + i
-        img = imageio.imread(img_name)
-        img_ = cv2.imread(img_name)
-        #plt.imshow(img_)
-        l=img.shape[0]
-        w=img.shape[1]
+    # for i in onlyfiles2:
+    #     towrite = './cleaned_train/PNEUMONIA/'
+    #     img_name = filename2 + i
+    #     img = imageio.imread(img_name)
+    #     img_ = cv2.imread(img_name)
+    #     #plt.imshow(img_)
+    #     l=img.shape[0]
+    #     w=img.shape[1]
         
-        img = img_.reshape(l,w,3)
-        print(img.shape)
-        result = dhe(img)
-        #plt.imshow(result)
-        #plt.show()
-        cv2.imwrite(towrite+i, result)
+    #     img = img_.reshape(l,w,3)
+    #     print(img.shape)
+    #     result = dhe(img)
+    #     #plt.imshow(result)
+    #     #plt.show()
+    #     cv2.imwrite(towrite+i, result)
 
     # for i in onlyfiles3:
     #     towrite = './cleaned_test/NORMAL/'
