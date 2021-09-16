ac6b7300ddb6241eadabbc5d0a4d3163343f8e4a
Abhibha Gupta <abhibha1807@gmail.com>
2021-09-16 17:12:13 +0530

    train 6 p

--- dhe.py
+++ dhe.py
@@ -216,47 +216,50 @@ def main():
     #     cv2.imwrite(towrite+i, result)
     # print('cleaned test PNEUMONIA finished')
     
-    c=0
-    print('started cleaning data train copy NORMAL')
-    for i in onlyfiles5:
-        c=c+1
-        if c>253:
-            towrite = './cleaned_train_copy/NORMAL/'
-            img_name = filename5 + i
-            img = imageio.imread(img_name)
-            img_ = cv2.imread(img_name)
-            #plt.imshow(img_)
-            l=img.shape[0]
-            w=img.shape[1]
-            
-            img = img_.reshape(l,w,3)
-            print(img.shape)
-            result = dhe(img)
-            #plt.imshow(result)
-            #plt.show()
-            cv2.imwrite(towrite+i, result)
-            print('written:', towrite+i)
-            print(c)
-    print('cleaned train copy NORMAL finished')
-    
     # c=0
-    # for i in onlyfiles6:
+    # print('started cleaning data train copy NORMAL')
+    # for i in onlyfiles5:
     #     c=c+1
-    #     towrite = './cleaned_train_copy/PNEUMONIA/'
-    #     img_name = filename6 + i
-    #     img = imageio.imread(img_name)
-    #     img_ = cv2.imread(img_name)
-    #     #plt.imshow(img_)
-    #     l=img.shape[0]
-    #     w=img.shape[1]
+    #     if c>253:
+    #         towrite = './cleaned_train_copy/NORMAL/'
+    #         img_name = filename5 + i
+    #         img = imageio.imread(img_name)
+    #         img_ = cv2.imread(img_name)
+    #         #plt.imshow(img_)
+    #         l=img.shape[0]
+    #         w=img.shape[1]
+            
+    #         img = img_.reshape(l,w,3)
+    #         print(img.shape)
+    #         result = dhe(img)
+    #         #plt.imshow(result)
+    #         #plt.show()
+    #         cv2.imwrite(towrite+i, result)
+    #         print('written:', towrite+i)
+    #         print(c)
+    # print('cleaned train copy NORMAL finished')
+    
+    c=0
+    print('started cleaning data train copy PNEUMONIA')
+    for i in onlyfiles6:
+        c=c+1
+        towrite = './cleaned_train_copy/PNEUMONIA/'
+        img_name = filename6 + i
+        img = imageio.imread(img_name)
+        img_ = cv2.imread(img_name)
+        #plt.imshow(img_)
+        l=img.shape[0]
+        w=img.shape[1]
         
-    #     img = img_.reshape(l,w,3)
-    #     print(img.shape)
-    #     result = dhe(img)
-    #     #plt.imshow(result)
-    #     #plt.show()
-    #     cv2.imwrite(towrite+i, result)
-    # print('cleaned train copy PNEUMONIA finished')
+        img = img_.reshape(l,w,3)
+        print(img.shape)
+        result = dhe(img)
+        #plt.imshow(result)
+        #plt.show()
+        cv2.imwrite(towrite+i, result)
+        print('written:', towrite+i)
+        print(c)
+    print('cleaned train copy PNEUMONIA finished')
 
     # c=0
     # for i in onlyfiles7:
